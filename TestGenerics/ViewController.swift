b8254e94925e0a563b57e39f30cb50326e84f9ac
Harshvardhan <harshvardhan@mobiuso.com>
2020-05-07 12:16:43 +0530

    Initial Commit

---
+++ TestGenerics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGenerics
+//
+//  Created by Harshvardhan Patidar on 07/05/20.
+//  Copyright © 2020 Harshvardhan Patidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
